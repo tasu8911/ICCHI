fb25e8887f3b66a6a87fcc05411118695febe239
tasu8911 <tasu658911@gmail.com>
2021-05-05 21:31:47 +0900

    vueの練習用todolist

---
+++ main.js
@@ -0,0 +1,75 @@
+var STORAGE_KEY = 'todos-vuejs-demo'
+var todoStorage = {
+  fetch: function() {
+    var todos = JSON.parse(
+      localStorage.getItem(STORAGE_KEY) || '[]'
+    )
+    todos.forEach(function(todo, index) {
+      todo.id = index
+    })
+    todoStorage.uid = todos.length
+    return todos
+  },
+  save: function(todos) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+  }
+}
+
+const app = new Vue({
+  el: '#app',
+  data: {
+    todos:[]
+  },
+  
+  watch: {
+    // オプションを使う場合はオブジェクト形式にする
+    todos: {
+      // 引数はウォッチしているプロパティの変更後の値
+      handler: function (todos) {
+        todoStorage.save(todos)
+      },
+      // deep オプションでネストしているデータも監視できる
+      deep: true
+    }
+  },
+
+    created() {
+    // インスタンス作成時に自動的に fetch() する
+    this.todos = todoStorage.fetch()
+  },
+
+  methods: {
+   // ToDo 追加の処理
+   doAdd: function(event, value) {
+    // ref で名前を付けておいた要素を参照
+    var comment = this.$refs.comment
+    // 入力がなければ何もしないで return
+    if (!comment.value.length) {
+      return
+    }
+    // { 新しいID, コメント, 作業状態 }
+    // というオブジェクトを現在の todos リストへ push
+    // 作業状態「state」はデフォルト「作業中=0」で作成
+    this.todos.push({
+      id: todoStorage.uid++,
+      comment: comment.value,
+      state: 0
+    })
+    
+    // フォーム要素を空にする
+    comment.value = ''
+  },
+
+  // ...
+    // 状態変更の処理
+    doChangeState: function(item) {
+      item.state = item.state ? 0 : 1
+    },
+    // 削除の処理
+    doRemove: function(item) {
+      var index = this.todos.indexOf(item)
+      this.todos.splice(index, 1)
+    }
+}
+  
+})
